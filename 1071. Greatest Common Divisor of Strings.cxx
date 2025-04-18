b117facd9f984b7f61c32dd0e4fd1702880cb6ff
Kiruthika U <kiruthikau0112@gmail.com>
2025-04-18 22:14:56 +0530

    Create 1071. Greatest Common Divisor of Strings.cxx

---
+++ 1071. Greatest Common Divisor of Strings.cxx
@@ -0,0 +1,17 @@
+/*
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+*/
+
+
+class Solution {
+public:
+
+
+    string gcdOfStrings(string str1, string str2) {
+        if (str1+str2==str2+str1){
+            return str1.substr(0,gcd(str1.size(),str2.size()));
+        }
+        return "";
+    }
+};
